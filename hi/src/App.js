8ab28d6986022ca82cbcb250ccb18adf31866c59
aditya113322 <215137246+aditya113322@users.noreply.github.com>
2025-08-14 15:21:22 +0530

    c

--- hi/src/App.js
+++ hi/src/App.js
@@ -3,13 +3,18 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="fun-text">
-      <span className="blush">😊</span> किसको बोलूँ हेलो, किसको बोलूँ हाय,
-      <br />
-      टेंशन भगाए बस अदरक वाली चाय! <span className="blush">🥰</span>
-      <br />
-      <span className="highlight">तो फिर… चाय पीने चले क्या? 😄</span>
-    <span>hi</span>
+    <div className="container">
+      <div className="fun-text">
+        <span className="blush">😊</span> किसको बोलूँ हेलो, किसको बोलूँ हाय,
+        <br />
+        टेंशन भगाए बस अदरक वाली चाय! <span className="blush">🥰</span>
+        <br />
+        <span className="highlight">तो फिर… चाय पीने चले क्या? 😄</span>
+      </div>
+
+      <div className="reply-box">
+        <span className="blush">😎</span> मे भी चलता हूँ छतरपुर! 🚗
+      </div>
     </div>
   );
 }
