241735a69d6896eff62a1295b48ddd34c0812445
aditya113322 <aditya.parwarish@gmail.com>
2025-08-16 09:53:31 +0100

    c

--- server/index-simple.js
+++ server/index-simple.js
@@ -10,7 +10,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["*","http://localhost:3000", "http://localhost:3001"],
     methods: ["GET", "POST"]
   }
 });
