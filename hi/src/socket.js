bf295a5c43d63e3bd4b06413e056e74193613bf9
aditya113322 <215137246+aditya113322@users.noreply.github.com>
2025-09-14 14:41:33 +0530

    c

--- hi/src/socket.js
+++ hi/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:5000"; // change when deployed
+const SOCKET_URL = "https://hehe2-1cz3.onrender.com"; // change when deployed
 
 export const socket = io(SOCKET_URL, {
   transports: ["websocket"],
